266ed6a82ce1c9b11ccccae0b48afc00fd6a17e7
Florent Delannoy <pluies@uponmyshoulder.com>
2010-08-28 02:38:58 -0400

    files cleanup

--- createDatabase.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists prices( price float(5.2), label varchar(120) );
-
-insert into prices(price, label) values(2.69,"Big Mac");
-insert into prices(price, label) values(2.89,"Quarter Pounder with cheese");
-insert into prices(price, label) values(1.89,"Big N' Tasty");
-insert into prices(price, label) values(3.69,"Double Quarter Pounder with cheese");
-insert into prices(price, label) values(3.09,"Chicken Selects (3pcs)");
-insert into prices(price, label) values(3.79,"10 Pc. Chicken McNuggets");
-insert into prices(price, label) values(2.49,"Filet-O-Fish");
